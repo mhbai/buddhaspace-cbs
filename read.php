ab08bd6bfb14058216c4c5d1f868324a85837e5f
mhbai <mhbai.tw@gmail.com>
2014-10-15 20:36:28 +0800

    check read function

--- read.php
+++ read.php
@@ -1,5 +1,7 @@
 <?php
-    $json=file_get_contents('data.json');
+    $json=file_get_contents('./data.json');
+    print $json;
+    $json=file_get_contents('./write.php');
     print $json;
     print "Hello World from read.php";
 ?>
